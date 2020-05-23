d6a220f03766a786543fd10c043360060e853b1e
romipaz <eccoromi@gmail.com>
2020-05-23 11:32:02 +0200

    add files v1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const hamburger = document.getElementById('hamburger');
+const navbarUL = document.getElementById('navbar-ul');
+
+hamburger.addEventListener('click', () => {
+    navbarUL.classList.toggle('show');
+})
