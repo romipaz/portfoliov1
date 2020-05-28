ba85ad3c07584b13d4b6040b5b0f385631119b3f
romipaz <eccoromi@gmail.com>
2020-05-28 10:49:14 +0200

    clean code

--- js/main.js
+++ js/main.js
@@ -28,20 +28,18 @@ sidebarLink.addEventListener("click", function () {
 
 
 about.addEventListener('click', function (evt) {
- const id = evt.target.dataset.id;
- if(id){
-     //remove active from other buttons
-     tabBtn.forEach(function(tabBtn) {
-         tabBtn.classList.remove("active");
-         evt.target.classList.add("active");
-     });
-     // hide articles with mismatch id
-     articles.forEach(function(article) {
-         article.classList.remove("active");
-     })
-     const element = document.getElementById(id);
-     element.classList.add("active");
-
-
- }
+    const id = evt.target.dataset.id;
+    if (id) {
+        //remove active from other buttons
+        tabBtn.forEach(function (tabBtn) {
+            tabBtn.classList.remove("active");
+            evt.target.classList.add("active");
+        });
+        // hide articles with mismatch id
+        articles.forEach(function (article) {
+            article.classList.remove("active");
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
 });
